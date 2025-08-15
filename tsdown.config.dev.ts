f1ad89229d428ffdc7f34a616173be34c41031ea
gk <promise_ghostking@163.com>
2025-08-16 00:36:43 +0800

    build: 打包配置

--- tsdown.config.dev.ts
+++ tsdown.config.dev.ts
@@ -1,29 +1,17 @@
 import type { UserConfig } from 'tsdown'
+import { readdirSync } from 'node:fs'
 import { defineConfig } from 'tsdown'
 
-export const config: UserConfig = [
-  {
-    platform: 'node',
-    format: 'esm',
-    dts: true,
-    minify: true,
-    clean: false,
-    sourcemap: false,
-    entry: './packages/cli/src/index.ts',
-    outDir: './packages/cli/dist',
-    watch: './packages/cli/src',
-  },
-  {
-    platform: 'node',
-    format: 'esm',
-    dts: true,
-    minify: true,
-    clean: false,
-    sourcemap: false,
-    entry: './packages/utils/src/index.ts',
-    outDir: './packages/utils/dist',
-    watch: './packages/utils/src',
-  },
-]
+export const config: UserConfig = readdirSync('./packages').map(dir => ({
+  platform: 'node',
+  format: 'esm',
+  dts: true,
+  minify: true,
+  clean: false,
+  sourcemap: false,
+  entry: `./packages/${dir}/src/index.ts`,
+  outDir: `./packages/${dir}/dist`,
+  watch: `./packages/${dir}/src`,
+}))
 
 export default defineConfig(config)
