f66a7ad956c50bcf8398a3384c256dc56a6c796c
gk <promise_ghostking@163.com>
2025-09-05 14:32:07 +0800

    fix: pkg 命令删除 types 多一个点问题

--- packages/cli/src/actions.ts
+++ packages/cli/src/actions.ts
@@ -289,7 +289,7 @@ export async function pkgAction(dir: string | undefined, _name: string | undefin
     await execa('pnpm.cmd', ['pkg', 'delete', 'scripts.test'], { cwd })
     await execa('pnpm.cmd', ['pkg', 'set', 'main=dist/index.js'], { cwd })
     await execa('pnpm.cmd', ['pkg', 'set', 'module=dist/index.js'], { cwd })
-    await execa('pnpm.cmd', ['pkg', 'set', 'types=dist/index..d.ts'], { cwd })
+    await execa('pnpm.cmd', ['pkg', 'set', 'types=dist/index.d.ts'], { cwd })
 
     mkdir(join(cwd, 'src'))
     writeFile(join(cwd, 'src', 'index.ts'), '', { flag: 'w' })
