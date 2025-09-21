d25511978863ec8ccd916d3c21ae2919ee187baf
gk <promise_ghostking@163.com>
2025-09-22 00:43:17 +0800

    refactor: 设置内置模板脚本

--- scripts/setDefaultTemplates.js
+++ scripts/setDefaultTemplates.js
@@ -1,7 +1,23 @@
-import { execSync } from 'node:child_process'
+import { exec, execSync } from 'node:child_process'
+import { cpSync, existsSync, mkdirSync } from 'node:fs'
+import { join } from 'node:path'
+import { cwd } from 'node:process'
 
-execSync('tm set packages/defaultTemplates/service service')
-execSync('tm set packages/defaultTemplates/pinia pinia')
-execSync('tm set packages/defaultTemplates/redux redux')
-execSync('tm set packages/defaultTemplates/vuex vuex')
-execSync('tm set packages/defaultTemplates/zustand zustand')
+exec('npm list -g @tmes/cli', (error) => {
+  if (error) {
+    const templatesPath = join(cwd(), 'packages', 'cli', 'dist', 'templates')
+    if (!existsSync(templatesPath)) {
+      mkdirSync(templatesPath)
+    }
+
+    cpSync(join(cwd(), 'packages', 'defaultTemplates'), templatesPath, { recursive: true })
+
+    return
+  }
+
+  execSync('tm set packages/defaultTemplates/service service')
+  execSync('tm set packages/defaultTemplates/pinia pinia')
+  execSync('tm set packages/defaultTemplates/redux redux')
+  execSync('tm set packages/defaultTemplates/vuex vuex')
+  execSync('tm set packages/defaultTemplates/zustand zustand')
+})
