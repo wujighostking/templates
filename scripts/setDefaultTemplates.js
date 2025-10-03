7ba182dd825a5554444dc6c5e6fe91207699ea55
gk <promise_ghostking@163.com>
2025-10-03 10:42:32 +0800

    build: 修改打包完成后执行脚本

--- scripts/setDefaultTemplates.js
+++ scripts/setDefaultTemplates.js
@@ -1,23 +1,14 @@
-import { exec, execSync } from 'node:child_process'
 import { cpSync, existsSync, mkdirSync } from 'node:fs'
 import { join } from 'node:path'
 import { cwd } from 'node:process'
 
-exec('npm list -g @tmes/cli', (error) => {
-  if (error) {
-    const templatesPath = join(cwd(), 'packages', 'cli', 'dist', 'templates')
-    if (!existsSync(templatesPath)) {
-      mkdirSync(templatesPath)
-    }
-
-    cpSync(join(cwd(), 'packages', 'defaultTemplates'), templatesPath, { recursive: true })
-
-    return
+function build() {
+  const templatesPath = join(cwd(), 'packages', 'cli', 'dist', 'templates')
+  if (!existsSync(templatesPath)) {
+    mkdirSync(templatesPath)
   }
 
-  execSync('tm set packages/defaultTemplates/service service')
-  execSync('tm set packages/defaultTemplates/pinia pinia')
-  execSync('tm set packages/defaultTemplates/redux redux')
-  execSync('tm set packages/defaultTemplates/vuex vuex')
-  execSync('tm set packages/defaultTemplates/zustand zustand')
-})
+  cpSync(join(cwd(), 'packages', 'defaultTemplates'), templatesPath, { recursive: true })
+}
+
+build()
