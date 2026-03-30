05d71a131ea0f6bbb55492e9dd011c60353e4e0d
gk <promise_ghostking@163.com>
2026-03-30 16:56:15 +0800

    feat: 添加创建项目命令的选项配置

--- packages/cli/src/commands/index.ts
+++ packages/cli/src/commands/index.ts
@@ -11,7 +11,16 @@ interface CommandConfig {
 export const commands: CommandConfig[] = [
   {
     command: ['create [name]', '创建一个新的项目', { allowUnknownOptions: true }],
-    options: [['--name, -n [name]', '需要创建的项目名称']],
+    options: [
+      ['--name, -n [name]', '需要创建的项目名称'],
+      ['--mode, -m [mode]', '需要创建的项目模式, 可选值: "monorepo" | "polyrepo"'],
+      ['--buildTool, -b [buildTool]', '需要创建的项目构建工具, 可选值: "vite" | "tsdown"'],
+      ['--type, -t [type]', '需要创建的项目类型, 可选值: "web" | "lib"'],
+      [
+        '--framework, -f [framework]',
+        '需要创建的项目框架, 可选值: "react" | "vue" | "nest" | "nuxt"',
+      ],
+    ],
     action: (name, options) => {
       const _name = normaizeName(name)
 
