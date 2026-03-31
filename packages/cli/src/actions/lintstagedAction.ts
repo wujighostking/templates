1fb33d9ac6c44878a51f2925dbe41024bcc48483
gk <promise_ghostking@163.com>
2026-03-31 10:34:21 +0800

    feat: 实现 lintstagedAction 函数，添加 lint-staged 配置支持

--- packages/cli/src/actions/lintstagedAction.ts
+++ packages/cli/src/actions/lintstagedAction.ts
@@ -1,1 +1,26 @@
-export function lintstagedAction() {}
+import { execa, isExists, join, log, pnpm, __dirname } from '@tmes/shared'
+
+import { addDevDependency } from '../config/dependencies'
+
+export async function lintstagedAction() {
+  const packagePath = join(__dirname, 'package.json')
+  const isPackageExists = isExists(packagePath)
+
+  if (!isPackageExists) {
+    log.error('package.json 文件不存在')
+    return
+  }
+
+  addDevDependency('lint-staged')
+
+  try {
+    await execa(pnpm, ['pkg', 'set', 'lint-staged.*=["pnpm lint:fix", "pnpm fmt"]', '--json'], {
+      cwd: __dirname,
+      stdio: 'inherit',
+    })
+
+    log.success('成功创建 lint-staged 配置')
+  } catch {
+    log.error('创建 lint-staged 配置失败')
+  }
+}
