ea0d5ef116b91ce56307f9a2f3e074f80c31babb
gk <promise_ghostking@163.com>
2026-03-31 12:19:24 +0800

    feat: 优化 commitlintAction 函数，添加依赖版本获取和处理逻辑

--- packages/cli/src/actions/commitlintAction.ts
+++ packages/cli/src/actions/commitlintAction.ts
@@ -1,20 +1,54 @@
 import { writeFile } from 'node:fs'
 import { EOL } from 'node:os'
 
-import { log } from '@tmes/shared'
+import { execa, log, pnpm, __dirname } from '@tmes/shared'
 
-import { addDevDependency } from '../config/dependencies'
+import { addDevDependency, getDependencies, getDevDependencies } from '../config/dependencies'
 
 const commitContext = "export default { extends: ['@commitlint/config-conventional'] }" + EOL
-export function commitlintAction() {
+export async function commitlintAction() {
   addDevDependency('@commitlint/cli', '@commitlint/config-conventional')
 
-  writeFile('commitlint.config.ts', commitContext, { encoding: 'utf-8' }, (err) => {
-    if (err) {
-      log.error('创建 commitlint 配置失败')
-      return
-    }
+  try {
+    const deps = await formatDepsVersion('devDependencies')
+    await execa(pnpm, ['pkg', 'set', ...deps], { cwd: __dirname })
 
-    log.success('成功创建 commitlint 配置')
-  })
+    writeFile('commitlint.config.ts', commitContext, { encoding: 'utf-8' }, (err) => {
+      if (err) {
+        log.error('创建 commitlint 配置失败')
+        return
+      }
+
+      log.success('成功创建 commitlint 配置')
+    })
+  } catch {
+    log.error('创建 commitlint 配置失败')
+  }
+}
+
+type DepsMode = 'devDependencies' | 'dependencies'
+
+function formatdeps(deps: string[], versions: string[], mode: DepsMode) {
+  return deps.map((dep, index) => `${mode}.${dep}=^${versions[index]}`)
+}
+
+async function formatDepsVersion(mode: DepsMode) {
+  const depsInstance = mode === 'devDependencies' ? getDevDependencies() : getDependencies()
+
+  try {
+    const promises = depsInstance.map(
+      async (dependency) =>
+        await execa(pnpm, ['view', dependency, 'version'], { cwd: __dirname }).then(
+          (res: any) => res.stdout,
+        ),
+    )
+
+    const versions = await Promise.all(promises)
+
+    const deps = formatdeps(depsInstance, versions, mode)
+    return deps
+  } catch {
+    log.error('获取依赖版本信息失败')
+    return []
+  }
 }
