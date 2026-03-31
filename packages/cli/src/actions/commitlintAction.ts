2aa39a93f2e6c7fb346badd871d63611d3e01f6f
gk <promise_ghostking@163.com>
2026-03-31 08:56:47 +0800

    chore: 更新配置与消除警告

--- packages/cli/src/actions/commitlintAction.ts
+++ packages/cli/src/actions/commitlintAction.ts
@@ -8,11 +8,13 @@ import { addDevDependency } from '../config/dependencies'
 const commitContext = "export default { extends: ['@commitlint/config-conventional'] }" + EOL
 export function commitlintAction() {
   addDevDependency('@commitlint/cli', '@commitlint/config-conventional')
+
   writeFile('commitlint.config.ts', commitContext, { encoding: 'utf-8' }, (err) => {
     if (err) {
       log.error('创建 commitlint 配置失败')
       return
     }
+
     log.success('成功创建 commitlint 配置')
   })
 }
--- packages/shared/src/chalk/index.ts
+++ packages/shared/src/chalk/index.ts
@@ -1,3 +1,4 @@
+// oxlint-disable no-console
 import chalk from 'chalk'
 
 export const warning = chalk.yellow
