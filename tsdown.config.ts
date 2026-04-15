2bd8eb506c6e87512fad6b05e982c2e8140cfeb2
gk <promise_ghostking@163.com>
2026-04-15 11:10:12 +0800

    chore: 更新 tsconfig.json 和 tsdown.config.ts，添加路径映射并根据环境设置 sourcemap

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'tsdown'
 
+const isDev =
+  process.env.NODE_ENV === 'development' || process.argv.some((arg) => arg.startsWith('--watch'))
+
 export default defineConfig({
   platform: 'node',
   entry: 'src/index.ts',
   format: 'esm',
   outDir: 'dist',
-  sourcemap: false,
+  sourcemap: isDev,
   watch: false,
   dts: true,
   minify: true,
