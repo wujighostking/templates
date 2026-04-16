8baccac0de4f285e81915625759edbbb36132a4d
gk <promise_ghostking@163.com>
2026-04-16 23:45:37 +0800

    feat: 更新 createAction 函数以支持异步项目名称输入，添加 getValue 函数以处理文本输入

--- packages/cli/src/actions/createAction.ts
+++ packages/cli/src/actions/createAction.ts
@@ -1,15 +1,21 @@
+import { getValue } from '@tmes/shared'
+
 interface Options {
   name: string
 }
 
-export function createAction(options: Options) {}
+export async function createAction(options: Options) {
+  let { name } = options
+
+  if (!name) {
+    name = (await getValue({
+      message: '请输入项目名称',
+      placeholder: '请输入...',
+      validate: (value) => (value ? undefined : '项目名称不能为空'),
+    })) as string
+  }
+}
 
 export function normaizeName(name: string | Options): Options {
-  if (typeof name === 'object') {
-    return name
-  }
-
-  return {
-    name,
-  }
+  return typeof name === 'object' ? name : { name }
 }
--- packages/shared/src/prompts/index.ts
+++ packages/shared/src/prompts/index.ts
@@ -1,4 +1,6 @@
-import { confirm } from '@clack/prompts'
+import { confirm, text, type TextOptions } from '@clack/prompts'
 
 export const shouldContinue = async (message: string) =>
   await confirm({ message, initialValue: false, inactive: '否', active: '是' })
+
+export const getValue = async (options: TextOptions) => await text(options)
