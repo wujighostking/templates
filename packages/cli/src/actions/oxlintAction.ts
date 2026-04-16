35ab510e215aca4999f322c5cd2da3a752dde71b
gk <promise_ghostking@163.com>
2026-04-16 09:18:37 +0800

    feat: 实现 oxlintAction 函数，添加 oxlint 配置生成逻辑

--- packages/cli/src/actions/oxlintAction.ts
+++ packages/cli/src/actions/oxlintAction.ts
@@ -1,1 +1,49 @@
-export function oxlintAction() {}
+import {
+  addDevDependency,
+  execa,
+  formatDepsVersion,
+  isExists,
+  join,
+  log,
+  pnpm,
+  __dirname,
+  writeFile,
+} from '@tmes/shared'
+
+export const oxlintConfigContext = `import { defineConfig } from 'oxlint'
+
+export default defineConfig({
+  rules: {
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+  },
+})
+`
+export async function oxlintAction() {
+  const packagePath = join(__dirname, 'package.json')
+  const isPackageExists = isExists(packagePath)
+
+  if (!isPackageExists) {
+    log.error('package.json 文件不存在')
+    return
+  }
+
+  addDevDependency('oxlint')
+
+  try {
+    const deps = await formatDepsVersion('devDependencies')
+    await execa(
+      pnpm,
+      ['pkg', 'set', 'scripts.lint=oxlint', 'scripts.lint:fix=oxlint --fix', ...deps],
+      {
+        cwd: __dirname,
+      },
+    )
+
+    await writeFile('oxlint.config.ts', oxlintConfigContext)
+
+    log.success('成功创建 oxlint 配置')
+  } catch {
+    log.error('创建 oxlint 配置失败')
+  }
+}
