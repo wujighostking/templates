a5bd747ad7e71e386fe6f71796df56d331e9a52a
gk <promise_ghostking@163.com>
2026-05-04 17:57:48 +0800

    test: 更新 commitlint 测试用例和 tsconfig 配置

--- packages/cli/__test__/actions/commitlint.spec.ts
+++ packages/cli/__test__/actions/commitlint.spec.ts
@@ -1,4 +1,4 @@
-import { mkdirSync } from 'fs'
+import { mkdirSync } from 'node:fs'
 import process from 'node:process'
 
 import { execa, join, pnpm, isExists, isWin, deleteFile } from '@tmes/shared'
@@ -8,10 +8,10 @@ beforeAll(async () => {
   await deleteFile(join(process.cwd(), './packages/cli/__test__/__temp/test-commitlint'))
 })
 
-describe('测试 tmes commitlint 命令', () => {
-  it('tmes commitlint', async () => {
-    const testCommitlintPath = join(process.cwd(), './packages/cli/__test__/__temp/test-commitlint')
+describe.sequential('测试 tmes commitlint 命令', () => {
+  const testCommitlintPath = join(process.cwd(), './packages/cli/__test__/__temp/test-commitlint')
 
+  it('tmes commitlint', async () => {
     if (!isExists(join(testCommitlintPath, 'package.json'))) {
       mkdirSync(testCommitlintPath, { recursive: true })
 
@@ -21,5 +21,26 @@ describe('测试 tmes commitlint 命令', () => {
     await execa(isWin() ? 'tmes.cmd' : 'tmes', ['commitlint'], { cwd: testCommitlintPath })
 
     expect(isExists(join(testCommitlintPath, 'commitlint.config.ts'))).toBeTruthy()
+  })
+
+  it('验证 conmitlint 版本信息', async () => {
+    const commitlintCliVersion = await execa(pnpm, ['view', '@commitlint/cli', 'version']).then(
+      (res) => res.stdout,
+    )
+
+    const commitlintConfigConventionalVersion = await execa(pnpm, [
+      'view',
+      '@commitlint/config-conventional',
+      'version',
+    ]).then((res) => res.stdout)
+
+    const result = await execa(pnpm, ['pkg', 'get', 'devDependencies'], {
+      cwd: testCommitlintPath,
+    }).then((res) => (res.stdout ? JSON.parse(res.stdout as string) : {}))
+
+    expect(result['@commitlint/cli'].slice(1)).toBe(commitlintCliVersion)
+    expect(result['@commitlint/config-conventional'].slice(1)).toBe(
+      commitlintConfigConventionalVersion,
+    )
   })
 })
