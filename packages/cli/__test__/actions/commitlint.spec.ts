0eec1826ca5f1853215e4853cc1d64bb846da61b
gk <promise_ghostking@163.com>
2026-05-07 15:06:26 +0800

    test: 修改测试用例不通过问题

--- packages/cli/__test__/actions/commitlint.spec.ts
+++ packages/cli/__test__/actions/commitlint.spec.ts
@@ -1,5 +1,4 @@
 import { mkdirSync } from 'node:fs'
-import process from 'node:process'
 import { fileURLToPath } from 'node:url'
 
 import { execa, join, pnpm, isExists, deleteFile, setDirname } from '@tmes/shared'
@@ -7,20 +6,21 @@ import { beforeAll, describe, expect, it } from 'vitest'
 
 import { commitlintAction } from '../../src/actions'
 
+const __dirname = fileURLToPath(import.meta.url)
+const testCommitlintPath = join(__dirname, '../../__temp/test-commitlint')
+
 beforeAll(async () => {
-  await deleteFile(join(process.cwd(), '../../__temp/test-commitlint'))
+  await deleteFile(testCommitlintPath)
 })
 
-describe.sequential('测试 tmes commitlint 命令', async () => {
-  const __dirname = fileURLToPath(import.meta.url)
-  const testCommitlintPath = join(__dirname, '../../__temp/test-commitlint')
-  if (!isExists(join(testCommitlintPath, 'package.json'))) {
-    mkdirSync(testCommitlintPath, { recursive: true })
-
-    await execa(pnpm, ['init'], { cwd: testCommitlintPath })
-  }
-
+describe('测试 tmes commitlint 命令', async () => {
   it('tmes commitlint', async () => {
+    if (!isExists(join(testCommitlintPath, 'package.json'))) {
+      mkdirSync(testCommitlintPath, { recursive: true })
+
+      await execa(pnpm, ['init'], { cwd: testCommitlintPath })
+    }
+
     setDirname(testCommitlintPath)
     await commitlintAction()
 
--- packages/cli/__test__/actions/oxc.spec.ts
+++ packages/cli/__test__/actions/oxc.spec.ts
@@ -1,5 +1,4 @@
 import { mkdirSync } from 'node:fs'
-import process from 'node:process'
 import { fileURLToPath } from 'node:url'
 
 import { deleteFile, execa, isExists, join, pnpm, setDirname } from '@tmes/shared'
@@ -7,21 +6,21 @@ import { beforeAll, describe, expect, it } from 'vitest'
 
 import { oxfmtAction, oxlintAction } from '../../src/actions'
 
+const __dirname = fileURLToPath(import.meta.url)
+const testOxcPath = join(__dirname, '../../__temp/test-oxc')
+
 beforeAll(async () => {
-  await deleteFile(join(process.cwd(), './__temp/test-oxc'))
+  await deleteFile(testOxcPath)
 })
 
-describe.sequential('测试 tmes oxfmt 和 tmes oxlint 命令', async () => {
-  const __dirname = fileURLToPath(import.meta.url)
-  const testOxcPath = join(__dirname, '../../__temp/test-oxc')
-
-  if (!isExists(join(testOxcPath, 'package.json'))) {
-    mkdirSync(testOxcPath, { recursive: true })
-
-    await execa(pnpm, ['init'], { cwd: testOxcPath })
-  }
-
+describe('测试 tmes oxfmt 和 tmes oxlint 命令', async () => {
   it('tmes oxfmt', async () => {
+    if (!isExists(join(testOxcPath, 'package.json'))) {
+      mkdirSync(testOxcPath, { recursive: true })
+
+      await execa(pnpm, ['init'], { cwd: testOxcPath })
+    }
+
     setDirname(testOxcPath)
     await oxfmtAction()
 
@@ -29,12 +28,6 @@ describe.sequential('测试 tmes oxfmt 和 tmes oxlint 命令', async () => {
   })
 
   it('tmes oxlint', async () => {
-    if (!isExists(join(testOxcPath, 'package.json'))) {
-      mkdirSync(testOxcPath, { recursive: true })
-
-      await execa(pnpm, ['init'], { cwd: testOxcPath })
-    }
-
     setDirname(testOxcPath)
     await oxlintAction()
 
