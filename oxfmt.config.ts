d3154013819c408afdf2f3657d98aebce6779b75
gk <promise_ghostking@163.com>
2026-07-21 15:06:04 +0800

    chore: 添加 .gitattributes 文件以自动处理换行符

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'oxfmt'
 
 export default defineConfig({
-  ignorePatterns: ['dist/**', '*.min.js', 'packages/templates/template-*'],
+  ignorePatterns: ['dist/**', '*.min.js', 'packages/templates/template-*', '.gitattributes'],
   semi: false,
   singleQuote: true,
   trailingComma: 'all',
